fd8939f4f8ba03ac6da3fba80edb5134aed36de9
darwin <202280141@psu.palawan.edu.ph>
2024-11-16 23:25:31 +0800

    MySql_adding _tables

--- api-programming-with-mysql.py
+++ api-programming-with-mysql.py
@@ -4,6 +4,21 @@ from http import HTTPStatus
 
 app = Flask(__name__)
 
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://username:password@localhost/books_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year
+        }
